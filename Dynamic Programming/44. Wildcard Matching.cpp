3012ba1675b39a7606afca9bc3c35253c9d38c02
Priyal Khandal <priyaalkhandal@gmail.com>
2026-06-24 08:57:10 +0530

    44

---
+++ Dynamic Programming/44. Wildcard Matching.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool solve(int i,int j,string& s,string& p,vector<vector<int>>&dp){
+        if(i == s.length() && j == p.length()){
+            return true;
+        }
+
+        if(j == p.length()){return false;}
+
+        if(i == s.length()){
+            while(j < p.length()){
+                if(p[j] != '*'){
+                    return false;
+                }
+                j++;
+            }
+            return true;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(s[i] == p[j] || p[j] == '?'){
+            return dp[i][j] = solve(i+1,j+1,s,p,dp);
+        }
+
+        if(p[j] == '*'){
+            return dp[i][j] = solve(i+1,j,s,p,dp) || solve(i,j+1,s,p,dp);
+        }
+        return dp[i][j] = false;
+    }
+    bool isMatch(string s, string p) {
+        int m = s.length(),n = p.length();
+
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+
+        return solve(0,0,s,p,dp);
+    }
+};
